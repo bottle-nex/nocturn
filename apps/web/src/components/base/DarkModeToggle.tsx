fdec93f51839908acf74da4756c475dbcae73b71
Anjan Suman <anjansuman80@gmail.com>
2025-10-01 22:03:03 +0530

    feat: added animated dark and light mode toggle button.

--- apps/web/src/components/base/DarkModeToggle.tsx
+++ apps/web/src/components/base/DarkModeToggle.tsx
@@ -1,38 +1,35 @@
-'use client';
-import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
-import { IoMdMoon } from 'react-icons/io';
-import { CiLight } from 'react-icons/ci';
-import ToolTipComponent from '../utility/TooltipComponent';
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import "@theme-toggles/react/css/Classic.css";
+import { Classic } from "@theme-toggles/react";
 
 export default function DarkModeToggle() {
+    const { resolvedTheme, setTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
+    useEffect(() => setMounted(true), []);
 
     if (!mounted) return null;
-    const toggleTheme = () => {
-        setTheme(theme === 'dark' ? 'light' : 'dark');
-    };
+
+    const isDark = resolvedTheme === "dark";
 
     return (
-        <ToolTipComponent content="Toggle between light and dark mode">
-            <div className="">
-                <button
-                    type="button"
-                    onClick={toggleTheme}
-                    className="flex items-center justify-center gap-2 px-3 py-2 dark:bg-neutral-600/30 bg-neutral-500/20 rounded-lg transition-all duration-200 transform hover:scale-105 !cursor-pointer"
-                >
-                    {theme === 'dark' ? (
-                        <CiLight className="text-xl" />
-                    ) : (
-                        <IoMdMoon className="text-lg" />
-                    )}
-                </button>
-            </div>
-        </ToolTipComponent>
+        <div
+            className="flex items-center gap-2 px-3 py-2 dark:bg-neutral-600/30 bg-neutral-500/20 rounded-lg transition-all duration-200 transform hover:scale-105 !cursor-pointer"
+        >
+            <Classic
+                toggled={isDark}
+                toggle={(next) => {
+                    const newVal =
+                        typeof next === "function" ? next(isDark) : next;
+                    setTheme(newVal ? "dark" : "light");
+                }}
+                duration={750}
+                className="text-xl "
+                {...({} as React.ComponentProps<typeof Classic>)}
+            />
+        </div>
     );
 }
