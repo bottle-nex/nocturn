031eefe94c6b81f79057238a1d5d242666274a6c
kant-github <kantrishi7779@gmail.com>
2026-01-09 05:13:08 +0530

    build fixes

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,22 +1,7 @@
 'use client';
-import { ReactNode, useMemo } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { clusterApiUrl } from '@solana/web3.js';
-import {
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-    AlphaWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+import { ReactNode } from 'react';
 
 export default function Providers({ children }: { children: ReactNode }) {
-    // use mainnet
-    const endpoint = clusterApiUrl('devnet'); // or "mainnet-beta"
-
-    const wallets = useMemo(
-        () => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new AlphaWalletAdapter()],
-        [],
-    );
-
     return (
         <div>
             {children}
