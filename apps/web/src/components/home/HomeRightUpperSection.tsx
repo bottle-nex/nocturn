862b7aa0213d960216d52fb3541f92d518802105
Anjansuman <anjansuman80@gmail.com>
2026-01-22 03:30:09 +0530

    creating recent ai creations card.

--- apps/web/src/components/home/HomeRightUpperSection.tsx
+++ apps/web/src/components/home/HomeRightUpperSection.tsx
@@ -6,11 +6,17 @@ import { Input } from '../ui/input';
 import { PiMagnifyingGlass } from 'react-icons/pi';
 import { v4 as uuid } from 'uuid';
 import { useRef } from 'react';
+import { cn } from '@/lib/utils';
+import { CgFileAdd } from "react-icons/cg";
+import ToolTipComponent from '../utility/TooltipComponent';
+import RecentAICreatedCard from '../ui/RecentAICreationCard';
+import { useAllQuizsStore } from '@/store/user/useAllQuizsStore';
 
 export default function HomeRightUpperSection() {
     const router = useRouter();
     const { session } = useUserSessionStore();
     const inputRef = useRef<HTMLInputElement>(null);
+    const { quizs } = useAllQuizsStore();
 
     function handleCreateNewQuiz() {
         router.push(`/new/${uuid()}`);
@@ -18,10 +24,54 @@ export default function HomeRightUpperSection() {
 
     return (
         <section className="flex items-center justify-between">
-            <div>
-                <span className="text-4xl text-black dark:text-white">
-                    Welcome {session?.user.name}!
-                </span>
+            <div></div>
+            <div className={cn(
+                "relative max-w-sm w-full h-11 rounded-[6px] ",
+                "border-neutral-800 dark:border-neutral-700 dark:bg-zinc-800 dark:text-white",
+                "focus:outline-none focus:border-neutral-800 dark:focus:border-neutral-600 focus-visible:ring-0 focus-visible:ring-offset-0",
+                )}>
+                <Input
+                    placeholder="Start creating quiz with AI..."
+                    className={cn(
+                        "h-full w-full pl-10  placeholder:text-gamma/40 dark:placeholder:text-neutral-500",
+                        "rounded-[6px]"
+                    )}
+                    ref={inputRef}
+                />
+                <PiMagnifyingGlass
+                    size={20}
+                    className="absolute top-1/2 left-3 -translate-y-1/2 text-neutral-500 dark:text-neutral-400 rounded-full"
+                />
+                <ToolTipComponent
+                    content={"upload pdf"}
+                    className='cursor-pointer'
+                >
+                    <CgFileAdd
+                        size={20} 
+                        className="absolute top-1/2 right-3 -translate-y-1/2 text-neutral-500 dark:text-neutral-400 rounded-full"
+                    />
+                </ToolTipComponent>
+
+                <div
+                    className={cn(
+                        'absolute z-50 top-12 w-full h-100 rounded-[6px] shadow-md px-2 py-3',
+                        'dark:bg-zinc-800 dark:text-white',
+                        'flex flex-col gap-y-2',
+                    )}
+                >
+                    <div className='text-xs px-2'>
+                        Recent AI creations
+                    </div>
+                    <div className='flex flex-col '>
+                        {quizs.map((q) => (
+                            <RecentAICreatedCard
+                                theme={q.theme}
+                                title={q.title}
+                                difficulty={5}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
             <div className="flex items-center justify-end gap-4">
                 <Button
@@ -31,17 +81,6 @@ export default function HomeRightUpperSection() {
                     <FiPlus />
                     <span>New Quiz</span>
                 </Button>
-                <div className="relative max-w-sm w-full h-11">
-                    <Input
-                        placeholder="Serch your quizzes.."
-                        className="border-neutral-800 dark:border-neutral-700 dark:bg-zinc-800 dark:text-white rounded h-full w-full pl-10 focus:outline-none focus:border-neutral-800 dark:focus:border-neutral-600 focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gamma/40 dark:placeholder:text-neutral-500"
-                        ref={inputRef}
-                    />
-                    <PiMagnifyingGlass
-                        size={20}
-                        className="absolute top-1/2 left-3 -translate-y-1/2 text-neutral-500 dark:text-neutral-400"
-                    />
-                </div>
             </div>
         </section>
     );
---
+++ apps/web/src/components/ui/RecentAICreationCard.tsx
@@ -0,0 +1,40 @@
+import { templates } from "@/lib/templates";
+import { cn } from "@/lib/utils";
+import EmptyCanvas from "../canvas/EmptyCanvas";
+
+interface RecentAICreatedCardProps {
+    theme: string,
+    title: string,
+    difficulty: number,
+}
+
+export default function RecentAICreatedCard({ theme, title, difficulty }: RecentAICreatedCardProps) {
+
+    const template = templates.find(t => t.id === theme);
+
+    return (
+        <div className="w-full h-16 flex gap-x-2 px-2 py-2 hover:bg-neutral-900 transition cursor-pointer rounded-[4px]">
+            {/* <div
+                className={cn(
+                        "h-full w-12 rounded-[4px] ",
+                    )}
+                style={{ backgroundColor: template?.background_color }}
+            ></div> */}
+            <EmptyCanvas
+                className="w-12 aspect-video outline-2 outline-black/40 dark:outline-white/40"
+                template={template!}
+            />
+            <div
+                className="flex flex-col justify-around " 
+            >
+                <div className="w-65 text-sm truncate">
+                    {title}
+                </div>
+                <div className="text-xs">
+                    <span className="text-neutral-300 ">difficulty {" "}</span>
+                    <span className="text-neutral-500 ">{difficulty}</span>
+                </div>
+            </div>
+        </div>
+    );
+}
