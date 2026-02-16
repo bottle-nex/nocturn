d9dd1c693ae785c31496926d865fa1ce6d33a0ac
kant-github <kantrishi7779@gmail.com>
2026-02-16 23:46:04 +0530

    fixed linting warnings

--- apps/web/src/lib/singleton-socket.ts
+++ apps/web/src/lib/singleton-socket.ts
@@ -4,9 +4,7 @@ let client: WebSocketClient | null = null;
 let currentQuizId: string | null = null;
 
 export function getWebSocketClient(quizId: string) {
-    console.log('creating socket using quiz id is : ', quizId);
     if (client && currentQuizId === quizId) {
-        console.log('returning client 1');
         return client;
     }
 
@@ -16,13 +14,11 @@ export function getWebSocketClient(quizId: string) {
     }
     client = new WebSocketClient(`ws://localhost:8080/ws?quizId=${quizId}`);
     currentQuizId = quizId;
-    console.log('returning client 2');
     return client;
 }
 
 export function cleanWebSocketClient() {
     if (client) {
-        console.log('closing the client connection');
         client.close();
     }
     client = null;
