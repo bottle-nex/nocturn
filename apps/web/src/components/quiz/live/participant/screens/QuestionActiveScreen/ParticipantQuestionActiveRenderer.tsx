8286f0db4acf37cb08d0e93ff8c97da8dc765ce6
Piyush Raj <piyush@Piyushs-MacBook-Air.local>
2026-03-03 23:11:12 +0530

    refactored participant question active screen

--- apps/web/src/components/quiz/live/participant/screens/QuestionActiveScreen/ParticipantQuestionActiveRenderer.tsx
+++ apps/web/src/components/quiz/live/participant/screens/QuestionActiveScreen/ParticipantQuestionActiveRenderer.tsx
@@ -3,6 +3,7 @@ import { useRef } from 'react';
 import { useLiveQuizStore } from '@/store/live-quiz/useLiveQuizStore';
 import ParticipantQuestionActiveOptions from './ParticipantQuestionActiveOptions';
 import TopTimerBarCompoment from '@/components/ui/TopTimerBarComponent';
+import AppLogo from '@/components/app/AppLogo';
 
 export default function ParticipantQuestionActiveRenderer() {
     const canvasRef = useRef<HTMLDivElement>(null);
@@ -18,7 +19,14 @@ export default function ParticipantQuestionActiveRenderer() {
 
     return (
         <div ref={canvasRef} className="w-full h-full flex justify-center items-center">
-            <section className="max-w-7xl mx-auto h-[80dvh] w-full rounded-xl relative overflow-hidden bg-white z-10 flex flex-col items-center p-18 gap-y-10">
+            <section className="max-w-7xl mx-auto h-[80dvh] w-full rounded-xl relative overflow-hidden bg-white/80 z-10 flex flex-col items-center p-18 gap-y-10">
+                <div className="absolute top-2 left-2">
+                    <AppLogo
+                        size={100}
+                        textColor="dark:text-dark-base text-dark-base -right-8"
+                        withText
+                    />
+                </div>
                 <TopTimerBarCompoment
                     startTime={gameSession.phaseStartTime!}
                     endTime={gameSession.phaseEndTime!}
