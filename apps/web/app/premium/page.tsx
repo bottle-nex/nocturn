8f418cfc99349220beaf2d45b9e710587de00cd7
Piyush Raj <piyush@Piyushs-MacBook-Air.local>
2026-03-05 19:35:03 +0530

    participant screen update

--- apps/web/app/premium/page.tsx
+++ apps/web/app/premium/page.tsx
@@ -2,7 +2,7 @@ import PremiumComponent from '@/components/premium/PremiumComponent';
 import PremiumPageMain from '@/components/premium/PremiumPageMain';
 import PremiumPricingCardComponent from '@/components/premium/PremiumPricingCardComponent';
 import LandingCTASection from '@/components/revamp/LandingCTASection';
-import LandingNavbar from '@/components/revamp/LandingNavbar';
+import LandingTestNav from '@/components/revamp/LandingTestNav';
 import LandingFooter from '@/components/test/LandingFooter';
 import { Metadata } from 'next';
 import { JSX } from 'react';
@@ -17,7 +17,7 @@ export default function PremiumPage(): JSX.Element {
     return (
         <div className="overflow-x-hidden w-full relative">
             <main className="min-h-screen w-full bg-light-base dark:bg-dark-base text-dark-base dark:text-light-base relative z-10 mb-[60vh] flex flex-col gap-y-15">
-                <LandingNavbar />
+                <LandingTestNav />
                 <PremiumPageMain />
                 <PremiumComponent />
                 <PremiumPricingCardComponent />
