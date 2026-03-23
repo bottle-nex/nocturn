261e42f369cf78fea8795d423357d8bbb3b3466a
Anjansuman <anjansuman80@gmail.com>
2026-03-23 22:55:37 +0530

    lint fixes.

--- apps/server/src/routes/collaborator.router.ts
+++ apps/server/src/routes/collaborator.router.ts
@@ -9,6 +9,11 @@ import authMiddleware from '../middlewares/auth.middleware';
 import Subscription from '../middlewares/subscription.middleware';
 
 // <---------------------- COLLABORATOR-ROUTES ---------------------->
-router.post('/quiz/invite-collaborator/:quizId', authMiddleware, Subscription.collaborator_limit, Collaborator.process);
+router.post(
+    '/quiz/invite-collaborator/:quizId',
+    authMiddleware,
+    Subscription.collaborator_limit,
+    Collaborator.process,
+);
 
 export default router;
