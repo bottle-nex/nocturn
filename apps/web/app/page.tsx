9e14b34b1de7646be6d3fab47d9b36d70b83f9c3
kant-github <kantrishi7779@gmail.com>
2026-05-07 19:28:05 +0530

    added something

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import PremiumSubscriptionCards from '@/components/premium/PremiumSubscriptionCards';
 import LandingCardsComponent from '@/components/refactor/LandingCardsComponent';
 import LandingCollaborateComponent from '@/components/refactor/LandingCollaborateComponent';
@@ -10,87 +9,30 @@ import LandingNavbarComponent from '@/components/refactor/LandingNavbarComponent
 import LandingUsdcSection from '@/components/refactor/LandingUsdcSection';
 import LandingUserType from '@/components/refactor/LandingUserType';
 import LandingCTASection from '@/components/revamp/LandingCTASection';
-import { Button } from '@/components/ui/button';
 import SectionDivider from '@/components/utility/SectionDivider';
-import { useEffect, useRef, useState } from 'react';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default function Page() {
-    const [open, setOpen] = useState(false);
-    const sectionRef = useRef<HTMLElement>(null);
-
-    function handleClick() {
-        setOpen(!open);
-    }
-
-    useEffect(() => {
-        function handleClickOutside(e: MouseEvent) {
-            if (sectionRef.current) {
-                // @ts-ignore
-                if (!sectionRef.current.contains(e.target)) {
-                    setOpen(false);
-                }
-            }
-        }
-
-        document.addEventListener("mousedown", handleClickOutside);
-    }, [])
-
     return (
-        <div className='h-screen w-screen flex items-center justify-center'>
-            <Button onClick={handleClick}>
-                click
-            </Button>
-            {
-                open == true ? (
-                    <section ref={sectionRef} className='h-80 w-80 bg-red-500'>
-                        rishi
-                    </section>
-                ) : (null)
-            }
+        <div className="min-h-screen w-full flex flex-col items-center gap-y-10 bg-light-alpha overflow-x-hidden">
+            <LandingNavbarComponent />
+            <LandingHeroSection />
+            <SectionDivider />
+            <LandingFeaturesComponent />
+            <SectionDivider />
+            <LandingCardsComponent />
+            <SectionDivider />
+            <LandingCollaborateComponent />
+            <SectionDivider />
+            <LandingUsdcSection />
+            <SectionDivider />
+            <LandingUserType />
+            <PremiumSubscriptionCards />
+            <SectionDivider />
+            <LandingFaqSection />
+            <SectionDivider />
+            <LandingCTASection />
+            <SectionDivider />
+            <LandingFooter />
         </div>
     );
 }
